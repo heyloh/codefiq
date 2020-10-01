7ac4484710b9718cc0a9571fbeedc05b176784e2
heyloh <lohana100739@gmail.com>
2020-09-30 22:42:23 -0300

    add Register Screen

---
+++ mobile/src/pages/Register/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Register = () => {
+  return( 
+    <View>
+      <Text>Tela de Login</Text>
+    </View>
+  );
+}
+
+export default Register;
