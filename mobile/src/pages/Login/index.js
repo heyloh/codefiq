b0d9221eb1f0632be709ee5d8ab0801437078e6a
heyloh <lohana100739@gmail.com>
2020-09-30 22:42:03 -0300

    add Login Screen

---
+++ mobile/src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Login = () => {
+  return( 
+    <View>
+      <Text>Tela de Login</Text>
+    </View>
+  );
+}
+
+export default Login;
