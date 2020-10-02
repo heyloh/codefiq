9b3179f9846e0e6a3a846c926479ad7b23c03d04
heyloh <lohana100739@gmail.com>
2020-10-02 16:19:59 -0300

    update Register Screen

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -1,13 +1,121 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import {
+  Image,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView
+} from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+import HeaderRegisterForm from '../../assets/images/HeaderRegisterForm.png';
+import Stairs from '../../assets/images/Stairs.png';
+
+import styles from './styles';
 
 const Register = () => {
-  return( 
-    <View>
-      <Text>Tela de Login</Text>
-    </View>
+  const { navigate } = useNavigation();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [avoidKeyboard, setAvoidKeyboard] = useState(false);
+
+  function handleNavigateToLogin() {
+    navigate('Login');
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={'position'}
+      style={styles.container}
+      enabled={avoidKeyboard}
+    >
+      <Image source={HeaderRegisterForm} style={styles.headerForm} />
+
+      <Image source={Stairs} style={styles.footerStairs} />
+
+      <View
+        style={styles.formContainer}
+      >
+        <View style={styles.formHeader}>
+          <Text style={styles.formTitle}>Crie uma conta</Text>
+          <TouchableOpacity
+            style={styles.formAltButton}
+            onPress={handleNavigateToLogin}
+          >
+            <Text style={styles.formAlt}>fazer login</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.labelInput}>Nome de usuário</Text>
+        <TextInput
+          style={styles.formInput}
+          placeholder="hilorena"
+          placeholderTextColor="#989EA6"
+          returnKeyType="next"
+          onFocus={() => setAvoidKeyboard(false)}
+          onChangeText={username => setUsername(username)}
+          defaultValue={username}
+        >
+
+        </TextInput>
+
+        <Text style={styles.labelInput}>E-mail</Text>
+        <TextInput
+          style={styles.formInput}
+          placeholder="lorena@email.com"
+          placeholderTextColor="#989EA6"
+          returnKeyType="next"
+          onFocus={() => setAvoidKeyboard(false)}
+          onChangeText={email => setEmail(email)}
+          defaultValue={email}
+        >
+
+        </TextInput>
+
+        <Text style={styles.labelInput}>Senha</Text>
+        <TextInput
+          style={styles.formInput}
+          placeholder="•••••"
+          placeholderTextColor="#989EA6"
+          returnKeyType="done"
+          autoCorrect={false}
+          secureTextEntry={true}
+          onFocus={() => setAvoidKeyboard(true)}
+          onChangeText={password => setPassword(password)}
+          defaultValue={password}
+        >
+
+        </TextInput>
+
+        <Text style={styles.labelInput}>Repita a senha</Text>
+        <TextInput
+          style={styles.formInput}
+          placeholder="•••••"
+          placeholderTextColor="#989EA6"
+          returnKeyType="done"
+          autoCorrect={false}
+          secureTextEntry={true}
+          onFocus={() => setAvoidKeyboard(true)}
+          onChangeText={password => setPassword(password)}
+          defaultValue={password}
+        >
+
+        </TextInput>
+
+        <TouchableOpacity
+          style={styles.buttonPrimary}
+        >
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </KeyboardAvoidingView>
   );
 }
 
---
+++ mobile/src/pages/Register/styles.js
@@ -0,0 +1,105 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#242530',
+    alignItems: 'center',
+  },
+  headerForm: {
+    width: 313,
+    height:152,
+    alignSelf: 'center',
+    marginTop: 10,
+    marginBottom: 10,  
+  },
+
+  formContainer:{
+    width: 365,
+    height: 540,       
+    marginBottom: 30,
+    borderRadius: 20,
+    backgroundColor: '#F0F0F7',
+  },
+
+  formHeader: {
+    maxWidth: 308,
+    height: 30,
+    marginLeft: 27,
+    marginTop: 25,
+    marginBottom: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  formTitle: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 24,
+    color: '#242530',
+  },
+  formAltButton:{
+    width: 98,
+    height: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5, 
+    borderWidth: 1.5,
+    borderColor: '#32ACBD',
+    marginTop: 8,
+  },
+  formAlt: {
+    fontFamily: 'Quicksand-Bold',
+    fontSize: 12,
+    color: '#32ACBD',
+  },
+
+  formInput:{
+    backgroundColor: '#F6F6FF',
+    borderColor: '#E6E6F0',
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 310,
+    height: 60,
+    alignSelf: 'center',
+    fontFamily: 'Quicksand-Regular',
+    fontStyle: 'normal',
+    fontSize: 14,
+    color: '#3C424E',
+    paddingLeft: 15,
+  },
+  labelInput: {
+    marginTop: 5,
+    marginBottom: 3,
+    fontFamily: 'Quicksand-Regular',
+    fontStyle: 'normal',
+    fontSize: 14,
+    color: '#989EA6',
+    paddingLeft: 30,
+  },
+
+  buttonText: {
+    fontFamily: 'Quicksand-Bold',
+    fontSize: 16,
+    color: '#EFF5F6',
+  },
+  buttonPrimary: {
+    marginTop: 21,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 310,
+    height: 60,
+    borderRadius: 10,
+    backgroundColor: '#32ACBD',
+  },
+
+  footerStairs: {
+    width: 242, 
+    height: 219, 
+    backgroundColor: 'transparent',
+    backfaceVisibility: 'hidden',
+    position: 'absolute',
+    right: -30,
+    bottom: -35
+  },
+});
