9a6a83b8285214e3ac083a95da355345d0909ad0
Arlene Pelenda <arlenej011@gmail.com>
2020-11-12 19:02:52 -0300

    Create courses table

---
+++ server/src/database/migrations/20201110214705-create-courses.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => { 
+    return queryInterface.createTable('courses', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primarykey: true,
+      },
+      name: {
+        type: Sequelize.STRING(10),
+        unique: true,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    return queryInterface.dropTable('courses');
+  }
+};
