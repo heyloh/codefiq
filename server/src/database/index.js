c886a93662089cd859c662a81e1d2e5e9416795e
Arlene Pelenda <arlenej011@gmail.com>
2020-11-13 18:34:57 -0300

    Add course model into index

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -3,11 +3,13 @@ const dbConfig = require('../config/database');
 
 /* Defining used models */
 const User = require('../models/User');
+const Course = require('../models/Course');
 
 /* Setting up the connection */
 const connection = new Sequelize(dbConfig);
 
 /* Telling model to use the connection above */
 User.init(connection);
+Course.init(connection);
 
 module.exports = connection;
