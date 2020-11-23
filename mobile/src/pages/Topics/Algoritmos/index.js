754d2450fa99df1ffe0c96121174cac79ded2d68
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-23 16:25:06 -0300

    ✨ Start Algoritmos screen

---
+++ mobile/src/pages/Topics/Algoritmos/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Algoritmos = () => {
+  return(
+    <View>
+      <Text>Algoritmos</Text>
+    </View>
+  );
+}
+
+export default Algoritmos;
