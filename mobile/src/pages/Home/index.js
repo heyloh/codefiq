d7a9917c4db95f7a441592304d83ea9c6f9d88a6
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-23 16:24:53 -0300

    ✨ Finish Home screen

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import CourseButton from '../../components/CourseButton';
 
 import styles from './styles';
 
 const Home = () => {
   return (
-    <View>
-      <Text>
-        Home
-      </Text>
-    </View>
+    <ScrollView  contentContainerStyle={styles.contentContainer}>
+      <Text style={styles.courseName}>Lógica de Programação</Text>
+      <CourseButton title="Algoritmos" isLocked={false} topic="Algoritmos" />
+      {/* <CourseButton title="Variáveis" topic="Variáveis" />
+      <CourseButton title="Objetos" topi/>
+      <CourseButton title="Operadores Lógicos" />
+      <CourseButton title="Condicionais" />
+      <CourseButton title="Estruturas de Repetição" />
+      <CourseButton title="Funções" /> */}
+    </ScrollView>
   );
 }
 
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -1,3 +1,17 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    backgroundColor: '#242530',
+    alignItems: 'center',
+    paddingBottom: 50
+  },
+
+  courseName: {
+    fontFamily: 'Quicksand-Bold',
+    fontSize: 24,
+    color: '#EFF5F6',
+    marginTop: 45,
+  },
+});
