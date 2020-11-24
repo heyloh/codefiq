c8d033d2bac93bdf2484bcf632d5c7382df9a700
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-24 20:48:02 -0300

    ✨ Add TopicFooter component

---
+++ mobile/src/components/TopicFooter/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#20212A',
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 75,
+    paddingRight: 75,
+  },
+  text: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    color: '#EFF5F6'
+  },
+  separator: {
+    width: 1,
+    height: 50,
+    backgroundColor: '#67A8C3'
+  }
+});
