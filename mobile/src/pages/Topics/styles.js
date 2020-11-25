7e3f18ca0a034cf3057b83734b39b065ec797f7a
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-25 15:25:36 -0300

    💄 Update Topics styles

---
+++ mobile/src/pages/Topics/styles.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#242530',
+    alignItems: 'center',
+    padding: 45,
+  },
+  text: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#C2C3CB',
+    maxWidth: 350,
+  },
+  textSec: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#C2C3CB',
+    maxWidth: 350,
+    marginTop: 20,
+  },
+  steps: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#685fa6',
+    alignSelf: 'flex-start',
+    maxWidth: 300,
+    marginTop: 10,
+  },
+  separator: {
+    width: 350,
+    height: 1,
+    backgroundColor: '#67A8C3',
+    marginTop: 30,
+  },
+  code: {
+    maxHeight: 300,
+    width: 370,
+    backgroundColor: '#20212A',
+    justifyContent: 'flex-start',
+    marginTop: 20,
+    paddingLeft: 20,
+    paddingBottom: 20,
+    borderRadius: 12,
+  },
+  highlight: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#685fa6',
+  },
+  pinkHighlight: {
+    fontFamily: 'Quicksand-Bold',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#ff33c1',
+  },
+  greenHighlight: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#77BA7D',
+  },
+  comment: {
+    fontFamily: 'Quicksand-Medium',
+    fontSize: 11,
+    lineHeight: 20,
+    color: '#9272c9'
+  }
+})
