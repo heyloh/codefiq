5af5fe3705eb48424b729f8aff4f0631cee05536
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-25 15:43:54 -0300

    🔥 Remove Popup

--- mobile/src/components/CourseButton/index.js
+++ mobile/src/components/CourseButton/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Dialog, { DialogContent, ScaleAnimation, DialogTitle, DialogFooter, DialogButton } from 'react-native-popup-dialog';
 
 import locked from '../../assets/icons/locked.png';
 import unlocked from '../../assets/icons/unlocked.png';
@@ -12,74 +11,34 @@ import { RectButton } from 'react-native-gesture-handler';
 
 const CourseButton = ({ isLocked = true, title, topic }) => {
   const { navigate } = useNavigation();
-  const [isVisible, setIsVisible] = useState(false);
 
   function handleNavigateToTopic() {
     navigate(topic);
   }
 
-  function handleShowPopup() {
-    setIsVisible(true);
-  }
-
-  function handleHidePopup() {
-    setIsVisible(false);
+  function handleReturn() {
+    return;
   }
 
   return (
     <View style={styles.container}>
       <RectButton 
         style={styles.button} 
-        onPress={ isLocked ? handleShowPopup : handleNavigateToTopic}
+        onPress={ isLocked ? handleReturn : handleNavigateToTopic}
       >
         <Text style={styles.course}>{title}</Text>
-        <Image source={arrowRight} style={styles.arrowRight}/>
+        { isLocked ? (
+          <View />
+        ) : (
+          <Image source={arrowRight} style={styles.arrowRight}/>
+        )}
+        
       </RectButton>
       { isLocked ? (
         <Image source={locked} style={styles.isLocked} />
       ) : (
         <Image source={unlocked} style={styles.isLocked} />
       ) }
-
-      <Dialog
-          visible={isVisible}
-
-          footer={
-            <DialogFooter style={styles.dialogFoot}>
-              <DialogButton
-                textStyle={styles.dialogTitleText}
-                text="Fiquei triste"
-                onPress={handleHidePopup}
-                style={styles.dialogButtonSad}
-              />
-              <DialogButton
-                textStyle={styles.dialogTitleText}
-                text="Tudo bem"
-                onPress={handleHidePopup}
-                style={styles.dialogButton}
-              />
-            </DialogFooter>
-          }
-
-          onTouchOutside={handleHidePopup}
-
-          dialogAnimation={new ScaleAnimation({
-            initialValue: 0,
-            useNativeDriver: true,
-          })}
-
-          dialogTitle={
-            <DialogTitle 
-              style={styles.dialogTitle} 
-              title="Tópico indisponível" 
-              textStyle={styles.dialogTitleText}
-            />
-          }
-        >
-          <DialogContent style={styles.dialogContent}>
-            <Text style={styles.text}>Estude os tópicos anteriores, em breve este estará disponível.</Text>
-          </DialogContent>
-        </Dialog>
     </View>
   );
 }
