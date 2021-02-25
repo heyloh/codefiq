4f98f2951582f82a0bdfbeb09c876b1290058775
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-24 22:06:21 -0300

    feat: update home structure

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, {useEffect, useState, useContext} from 'react';
+import {Text, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import CourseButton from '../../components/CourseButton';
+
+import {UserContext} from '../../contexts/UserContext';
 
 import api from '../../services/api';
 
 import styles from './styles';
 
 const Home = () => {
+  const {isLoggedIn} = useContext(UserContext);
   const [subjects, setSubjects] = useState([]);
 
   useEffect(() => {
-    api.get('/subjects').then(response => {
+    api.get('/subjects').then((response) => {
       setSubjects(response.data);
     });
-  });
+  }, [isLoggedIn === true]);
 
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <Text style={styles.courseName}>Lógica de Programação</Text>
-        {subjects.map(subject => {
+        {subjects.map((subject) => {
           return (
             <CourseButton
               key={subject.id}
@@ -28,11 +31,11 @@ const Home = () => {
               isLocked={false}
               topic={`${subject.name}Stack`}
             />
-          )
+          );
         })}
       </ScrollView>
     </View>
   );
-}
+};
 
 export default Home;
