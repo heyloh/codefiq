1603dd0a7bb6b90d92623b311cde7a3b7f25c3cc
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-24 22:05:50 -0300

    feat: handle clicks

--- mobile/src/components/Option/index.js
+++ mobile/src/components/Option/index.js
@@ -1,27 +1,52 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import React, {useState, useEffect, useContext} from 'react';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
 
-const Option = ({text, isCorrect}) => {
+import {UserContext} from '../../contexts/UserContext';
+
+import wrong from '../../assets/icons/wrong.png';
+import right from '../../assets/icons/right.png';
+
+const Option = ({text, isCorrect, exerciseId}) => {
   const [showResult, setShowResult] = useState(false);
+  const {clickExercise, verifyIfEnabled, clickedExercises} = useContext(
+    UserContext,
+  );
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    clicked ? null : handleShowResult();
+  }
 
   function handleShowResult() {
     setShowResult(true);
   }
 
-  return(
-    <RectButton style={styles.button} onPress={handleShowResult}>
-      <Text style={styles.text}>
-        {text}
-      </Text>
-      { showResult ? (
+  useEffect(() => {
+    setClicked(clickedExercises[exerciseId] ? true : false);
+  }, [clickedExercises]);
+
+  return (
+    <RectButton
+      enabled={verifyIfEnabled(exerciseId)}
+      style={styles.button}
+      onPress={handleClick}>
+      <View style={styles.view}>
+        <Text style={styles.text}>{text}</Text>
+        {showResult ? (
+          <Image style={styles.image} source={isCorrect ? right : wrong} />
+        ) : (
+          <View />
+        )}
+      </View>
+      {/* {showResult ? (
         <View style={isCorrect ? styles.correct : styles.incorrect} />
       ) : (
         <View />
-      ) }
+      )} */}
     </RectButton>
   );
-}
+};
 
 const styles = StyleSheet.create({
   button: {
@@ -31,13 +56,21 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'center',
     marginTop: 20,
+  },
+  view: {
+    width: 300,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   text: {
     fontFamily: 'Quicksand-Medium',
     fontSize: 10,
     color: '#EFF5F6',
-    maxWidth: 270,
+    maxWidth: 260,
+    marginLeft: 10,
   },
 
   correct: {
@@ -45,15 +78,22 @@ const styles = StyleSheet.create({
     height: 3,
     backgroundColor: '#77BA7D',
     position: 'absolute',
-    top: 0
+    top: 0,
   },
   incorrect: {
     width: 290,
     height: 3,
     backgroundColor: '#C35252',
     position: 'absolute',
-    top: 0
+    top: 0,
   },
-})
+
+  image: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+});
 
 export default Option;
