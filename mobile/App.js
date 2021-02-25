90abda2f35f87c8b839df18eb3116f1c28a8587b
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-24 22:02:15 -0300

    feat: use user context provider

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,26 @@
 import 'react-native-gesture-handler';
 
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
+
+import {UserProvider} from './src/contexts/UserContext';
 
 import AppStack from './src/routes/AppStack.js';
 
 import SplashScreen from 'react-native-splash-screen';
 
-
 const App = () => {
-
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
-    <>
+    <UserProvider>
       <StatusBar backgroundColor="#20212B" barStyle="light-content" />
       <AppStack />
-    </>
+    </UserProvider>
   );
 };
-  
+
 export default App;
