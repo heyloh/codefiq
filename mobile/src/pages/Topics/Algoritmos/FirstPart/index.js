90a05fe404ecb7611f6edf540f9d0067d45b5c57
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-24 22:04:21 -0300

    feat: show algorithms exercises from db

--- mobile/src/pages/Topics/Algoritmos/FirstPart/index.js
+++ mobile/src/pages/Topics/Algoritmos/FirstPart/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import Option from '../../../../components/Option';
-import Question from '../../../../components/Question';
+import {Text, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import StepByStep from '../../../../components/StepByStep';
 import TopicFooter from '../../../../components/TopicFooter';
 
 import styles from '../../styles';
@@ -15,83 +14,58 @@ const FirstPart = () => {
           Através dessas lições você será capaz de entender um pouco sobre os
           princípios por trás dos códigos.
         </Text>
-        <Question title="Mas antes, o que seria um algoritmo?" />
-        <Option
-          text="Texto em linguagem humana usado para realizar comandos em um dispositivo."
-          isCorrect={false}
-        />
-        <Option
-          text="Texto ilegível por humanos usado para realizar comandos em um dispositivo."
-          isCorrect={false}
-        />
-        <Option
-          text="Texto escrito em uma linguagem de programação usado para realizar comandos em um dispositivo."
-          isCorrect={true}
-        />
-        <Option
-          text="Sequência de números em base hexadecimal usada para realizar comandos em um dispositivo."
-          isCorrect={false}
-        />
-
-        <View style={styles.separator} />
 
         <Text style={styles.textSec}>
-          Assim como cada página de um livro contém diversas frases onde cada 
-          palavra tem seu significado e objetivo, o código tem diversas partes, 
-          e cada uma realiza algum tipo de comando, e assim como um conjunto de 
+          Assim como cada página de um livro contém diversas frases onde cada
+          palavra tem seu significado e objetivo, o código tem diversas partes,
+          e cada uma realiza algum tipo de comando, e assim como um conjunto de
           páginas forma um livro, um conjunto de códigos forma um programa.
         </Text>
-        <Text style={styles.textSec}> 
-          Linguagens de programação são usadas para basicamente escrever um 
-          algoritmo. Algoritmos são transformados em um código legível pela 
-          máquina, direta ou indiretamente, de alguma forma que muda de acordo 
-          com a linguagem, processo este chamado de compilação. 
+        <Text style={styles.textSec}>
+          Linguagens de programação são usadas para basicamente escrever um
+          algoritmo. Algoritmos são transformados em um código legível pela
+          máquina, direta ou indiretamente, de alguma forma que muda de acordo
+          com a linguagem, processo este chamado de compilação.
         </Text>
         <Text style={styles.textSec}>
-          Softwares muito complexos usados todos os dias em computadores, 
-          celulares, tablets e outros, são possíveis graças a uma grande 
+          Softwares muito complexos usados todos os dias em computadores,
+          celulares, tablets e outros, são possíveis graças a uma grande
           quantidade de códigos, onde cada parte realiza uma ação mais simples.
         </Text>
-
-        <Question title="Quem escreve o algoritmo?" />
-        <Option 
-          text="O programador"
-          isCorrect={true}
-        />
-        <Option 
-          text="O usuário comum"
-          isCorrect={false}
-        />
-        <Option 
-          text="A própria máquina, usando de base o código"
-          isCorrect={false}
-        />
-        <Option 
-          text="O algoritmo não é escrito"
-          isCorrect={false}
-        />
-
-        <Question title="O que é o código de máquina?" />
-        <Option 
-          text="Um texto ilegível por humanos que o programador não pode gerar nem indiretamente."
-          isCorrect={false}
-        />
-        <Option 
-          text="Um texto escrito diretamente pelo programador, que pode ser diretamente executado pela máquina após ser escrito."
-          isCorrect={false}
-        />
-        <Option 
-          text="Uma série de comandos codificados que o computador entende de forma direta."
-          isCorrect={true}
-        />
-        <Option 
-          text="Uma forma de codificar comandos usada diretamente pelo programador através de linguagens de programação."
-          isCorrect={false}
-        />
+        <Text style={styles.textSec}>
+          Um algoritmo pode ser entendido basicamente como um conjunto de passos
+          para se chegar a um certo resultado. É bem simples entender a lógica
+          de um algoritmo, afinal sua estrutura tem diversas correspondências no
+          mundo real.
+        </Text>
+        <Text style={styles.textSec}>
+          Se tivéssemos que fazer pipoca a partir de um pacote e utilizando um
+          micro-ondas, como seriam os passos? Provavelmente algo como:
+        </Text>
+        <StepByStep value={1} text="Abrir o micro-ondas" />
+        <StepByStep value={2} text="Colocar o pacote" />
+        <StepByStep value={3} text="Definir um tempo" />
+        <StepByStep value={4} text="Ligar o micro-ondas" />
+        <StepByStep value={5} text="Retirar ao ouvir o beep" />
+        <Text style={styles.textSec}>
+          Um algoritmo não tem uma estrutura tão diferente, se trata de comandos
+          basicamente, que são separados por linhas e que trabalham juntos
+          resultado em algo no final da execução.
+        </Text>
+        <Text style={styles.textSec}>
+          A partir de algoritmos pode-se fazer um programa simples que trás o
+          resultado da soma de todos os números entre 1 e 100 ou um código que
+          faz um website inteiro e bem completo funcionar.
+        </Text>
+        <Text style={styles.textSec}>
+          A forma de escrever um, executar, modificar, formatar e etc, muda de
+          acordo com a linguagem de programação a ser usada, então não vamos
+          entrar em detalhes no momento.
+        </Text>
       </ScrollView>
       <TopicFooter nextScreen="SecondPart" />
     </>
   );
-}
+};
 
 export default FirstPart;
--- mobile/src/pages/Topics/Algoritmos/SecondPart/index.js
+++ mobile/src/pages/Topics/Algoritmos/SecondPart/index.js
@@ -1,70 +1,56 @@
-import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
+import {ScrollView, View} from 'react-native';
 import Option from '../../../../components/Option';
 import Question from '../../../../components/Question';
-import StepByStep from '../../../../components/StepByStep';
 import TopicFooter from '../../../../components/TopicFooter';
+
+import api from '../../../../services/api';
 
 import styles from '../../styles';
 
 const SecondPart = () => {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    api.get(`/exercises?subject_id=6`).then((response) => {
+      setExercises(response.data);
+    });
+  });
+
   return (
     <>
       <ScrollView contentContainerStyle={styles.container}>
-        <Question title="Podemos entender um algoritmo, de forma bem simples, como sendo:" />
-        <Option
-          text="Uma sequência de comandos que atende a um certo propósito."
-          isCorrect={true}
-        />
-        <Option
-          text="Necessariamente um programa complexo que só será criado através de supercomputadores."
-          isCorrect={false}
-        />
-        <Option
-          text="Um código diretamente legível e executável pela máquina."
-          isCorrect={false}
-        />
-        <Option
-          text="Uma operação matemática que realiza o processo inverso da exponenciação."
-          isCorrect={false}
-        />
-
-        <View style={styles.separator} />
-
-        <Text style={styles.textSec}>
-          Um algoritmo pode ser entendido basicamente como um conjunto de 
-          passos para se chegar a um certo resultado. 
-          É bem simples entender a lógica de um algoritmo, afinal sua estrutura 
-          tem diversas correspondências no mundo real.   
-        </Text>
-        <Text style={styles.textSec}>
-          Se tivéssemos que fazer pipoca a partir de um pacote e utilizando um 
-          micro-ondas, como seriam os passos? Provavelmente algo como:
-        </Text>
-        <StepByStep value={1} text="Abrir o micro-ondas" />
-        <StepByStep value={2} text="Colocar o pacote" />
-        <StepByStep value={3} text="Definir um tempo" />
-        <StepByStep value={4} text="Ligar o micro-ondas" />
-        <StepByStep value={5} text="Retirar ao ouvir o beep" />
-        <Text style={styles.textSec}>
-          Um algoritmo não tem uma estrutura tão diferente, se trata de 
-          comandos basicamente, que são separados por linhas e que trabalham 
-          juntos resultado em algo no final da execução.
-        </Text> 
-        <Text style={styles.textSec}>
-          A partir de algoritmos pode-se fazer um programa simples que trás o 
-          resultado da soma de todos os números entre 1 e 100 ou um código que 
-          faz um website inteiro e bem completo funcionar. 
-        </Text>
-        <Text style={styles.textSec}>
-          A forma de escrever um, executar, modificar, formatar e etc, muda de 
-          acordo com a linguagem de programação a ser usada, então não vamos 
-          entrar em detalhes no momento.
-        </Text>
+        {exercises.map((exercise) => {
+          return (
+            <View key={exercise.id}>
+              <Question title={exercise.content.question} />
+              <Option
+                exerciseId={exercise.id}
+                text={exercise.content.options[0].label1}
+                isCorrect={exercise.content.options[0].correct ? true : false}
+              />
+              <Option
+                exerciseId={exercise.id}
+                text={exercise.content.options[1].label2}
+                isCorrect={exercise.content.options[1].correct ? true : false}
+              />
+              <Option
+                exerciseId={exercise.id}
+                text={exercise.content.options[2].label3}
+                isCorrect={exercise.content.options[2].correct ? true : false}
+              />
+              <Option
+                exerciseId={exercise.id}
+                text={exercise.content.options[3].label4}
+                isCorrect={exercise.content.options[3].correct ? true : false}
+              />
+            </View>
+          );
+        })}
       </ScrollView>
-      <TopicFooter nextScreen="FinishTopic" topic="Algoritmos"/>
+      <TopicFooter nextScreen="FinishTopic" topic="Algoritmos" />
     </>
   );
-}
+};
 
 export default SecondPart;
