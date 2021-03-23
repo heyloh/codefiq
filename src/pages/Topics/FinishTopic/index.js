b9702d7b94f3f4e71daa8f31f19bf082c22ed046
Lohana Torres <lt.leopoldo@gmail.com>
2021-03-23 20:36:18 -0300

    feat: show result of topic

--- src/pages/Topics/FinishTopic/index.js
+++ src/pages/Topics/FinishTopic/index.js
@@ -6,28 +6,49 @@ import {useNavigation} from '@react-navigation/native';
 import {UserContext} from '../../../contexts/UserContext';
 
 import checkCircle from '../../../assets/icons/check-circle.png';
+import wrong from '../../../assets/icons/wrong.png';
 
 const FinishTopic = ({route}) => {
   const {topic} = route.params;
   const {navigate} = useNavigation();
-  const {resetClickedExercises} = useContext(UserContext);
+  const {resetClickedExercises, answers, resetAnswers} = useContext(
+    UserContext,
+  );
 
   function handleFinish() {
     resetClickedExercises();
+    resetAnswers();
     navigate('Home');
   }
 
   return (
     <View style={styles.container}>
-      <Image source={checkCircle} style={styles.success} />
-      <Text style={styles.title}>Parabéns!</Text>
-      <Text style={styles.text}>
-        Você acabou de concluir o tópico de {topic}. Siga em frente e continue
-        aprendendo!
-      </Text>
-      <RectButton style={styles.button} onPress={handleFinish}>
-        <Text style={styles.buttonText}>Continuar</Text>
-      </RectButton>
+      {answers.correct > answers.incorrect ? (
+        <>
+          <Image source={checkCircle} style={styles.success} />
+          <Text style={styles.title}>Parabéns!</Text>
+          <Text style={styles.text}>
+            Você acabou de concluir o tópico de {topic}. Siga em frente e
+            continue aprendendo!
+          </Text>
+          <RectButton style={styles.button} onPress={handleFinish}>
+            <Text style={styles.buttonText}>Continuar</Text>
+          </RectButton>
+        </>
+      ) : (
+        <>
+          <Image source={wrong} style={styles.success} />
+          <Text style={styles.title}>Que pena!</Text>
+          <Text style={styles.text}>
+            Você não conseguiu concluir o tópico de {topic}. Tente novamente,
+            parece que você errou muitos exercícios, continue praticando até
+            conseguir.
+          </Text>
+          <RectButton style={styles.button} onPress={handleFinish}>
+            <Text style={styles.buttonText}>Continuar</Text>
+          </RectButton>
+        </>
+      )}
     </View>
   );
 };
